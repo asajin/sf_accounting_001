ba73d8cbf1e372f7153a6833d301f1abcafbb2e8
Sajin Andrei <sajinandrei@yahoo.fr>
2013-06-15 19:31:57 +0300

    #14 Common data supplier - generate crud for supplier entity

---
+++ src/Common/DataBundle/Form/SupplierType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Common\DataBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class SupplierType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('address')
+            ->add('description')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Common\DataBundle\Entity\Supplier'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'common_databundle_suppliertype';
+    }
+}
